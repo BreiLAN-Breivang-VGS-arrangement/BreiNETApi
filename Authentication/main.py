b9ebbfa981524a35089e7f43c07d816e877899a8
Quezty <storlijoachim@gmail.com>
2024-04-04 09:56:26 +0200

    finished config setup

--- Authentication/main.py
+++ Authentication/main.py
@@ -1,11 +1,27 @@
-from flask import request, jsonify
+from flask import request, jsonify, Flask
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
 from config import app, db
-from models import Contacts
+from models import Users
 
-@app.route('/Authentication')
-def method_name():
-    users = Users.query.all()
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@login_manager.user_loader
+def loader_user():
+    return db.session.get(Users, user_id)
+
+@app.route('/checkauth', methods=['POST'])
+def Check_auth():
+    pass
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    pass
 
 if __name__ == "__main__":
     with app.app_context():
