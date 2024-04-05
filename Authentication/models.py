2593ba8ccede77a3841ca301b9e87310944d2620
Sebastian Olsson <sebas-o@online.no>
2024-04-05 18:09:22 +0200

    removed old config and models

--- Authentication/models.py
+++
@@ -1,8 +0,0 @@
-from config import db
-from flask_login import UserMixin
-
-class Users(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    username = db.Column(db.String(40), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-    clearance = db.Column(db.Integer, unique=False, nullable=False)
