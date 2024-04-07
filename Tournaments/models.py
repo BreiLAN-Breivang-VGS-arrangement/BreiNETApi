2dc31f912b45fd6d0ebec80ca82a1ab34a5c8997
Sebastian Olsson <sebas-o@online.no>
2024-04-07 16:13:51 +0200

    fixed tourney, i think...

--- Tournaments/models.py
+++ Tournaments/models.py
@@ -4,6 +4,6 @@ class Tournaments(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
     name = db.Column(db.String(40), unique=True, nullable=False)
     time = db.Column(db.String(40), unique=False, nullable=False)
-    date = db.Column(db.DATE, unique=False, nullable=False)
+    date = db.Column(db.String, unique=False, nullable=False)
     host = db.Column(db.String, unique=False, nullable=False)
     link = db.Column(db.String, unique=False, nullable=False)
